4513894e02b882e0e5aaa538c39c68ea4ca87914
tomhowarth76 <tomhowarth@mac.com>
2019-10-30 14:26:09 +0000

    first commit of the tom vince parnership

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,9 @@
 require './lib/docking_station'
 
 describe DockingStation do
+it "reposonds to the method release" do
+  docking_station = DockingStation.new
 
+  is_expected.to respond_to :release_bike
+end
 end
