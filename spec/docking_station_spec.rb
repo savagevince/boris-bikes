67dac95e5b33e18768c72a34c0c7d7e1a9ff93d1
VINCENT SAVAGE <savagevince@hotmail.com>
2019-10-30 16:30:31 +0000

    Tom & Vince and Mystery of The Docking Station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,19 @@
 require './lib/docking_station'
 
 describe DockingStation do
-it "reposonds to the method release" do
+  it "reposonds to the method release" do
   docking_station = DockingStation.new
 
   is_expected.to respond_to :release_bike
-end
+  end
+  it 'allows a bike to be docked'do
+    bike = Bike.new
+    station = DockingStation.new
+
+    is_expected.to respond_to :bike_dock
+  end
+  it 'sees a docked bike' do
+
+    expect(bike_in_dock).to be true
+  end
 end
